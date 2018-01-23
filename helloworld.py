71eb0c3ced42105801865668315cb67e75627e32
GnZ <ksganesh19@users.noreply.github.com>
2018-01-23 13:07:33 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -50,6 +50,18 @@ print(23/5)#older versions used to return only the integer.
 print(23.0//5) #fore division operator
 print(25.0%2) # if only float in either of the numbers, we will get 1.0 else 1
 print(25%2)
+## hierarchy/Precedence (), **,*,/,%,+,-
 print("###################")
-## hierarchy (), **,*,/,%,+,-
 ##################################################
+#binary number manipulation
+##bitwise operator <<, >> , &(bitwise operator)
+print (1 << 3)
+##So, 000001 becomes 0001000. Prints 8. Moves to 3 places to left
+print(32 >> 5)
+##So, 100000 becomes 000001. Prints 1. Moves to 5 places to right
+print(255 & 256)
+##Compares 0111111111 and 1000000000. Since, each bit differs. So, it returns 0. If matches, it gives the number 256. & operator 1,0 gives 0/, | operator gives 1,0 as 1. ^ -XOR, gives 1,0 as 1 and 1,1, as 0
+print("###################")
+##################################################
+#string manipulation
+##len() function
