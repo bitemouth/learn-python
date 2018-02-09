c66b1afa301260f3de1f45e149f855103b1bd8c7
GnZ <ksganesh19@users.noreply.github.com>
2018-02-09 12:04:54 +0530

    updated changes to helloworld

--- helloworld.py
+++ helloworld.py
@@ -60,8 +60,58 @@ print (1 << 3)
 print(32 >> 5)
 ##So, 100000 becomes 000001. Prints 1. Moves to 5 places to right
 print(255 & 256)
+print(255 | 256)
+print(12 ^ 3)
 ##Compares 0111111111 and 1000000000. Since, each bit differs. So, it returns 0. If matches, it gives the number 256. & operator 1,0 gives 0/, | operator gives 1,0 as 1. ^ -XOR, gives 1,0 as 1 and 1,1, as 0
 print("###################")
 ##################################################
 #string manipulation
 ##len() function
+## 0 and -1 from right
+##immutable types. we can't assign stringName[-2]='H'
+varName = 'checkmate'
+print(varName[0])
+print(varName[-1])
+print(varName[2:4])
+print(varName[1:])
+##if missed from beginning
+##second parameter is length
+##first parameter is the starting character
+print("###################")
+##################################################
+#concatenation in Strings
+print('yes or '+'no')
+print(100* 'ya')
+word="ward"
+print('L' + word[1:])
+##no need of + if it is direct values. if variables are used + is needed.
+print('this' 'that') #prints thisthat
+a='this'
+b='that'
+##print(a b) #throws error
+print("###################")
+##################################################
+#format method
+print("I am {0} and my company is {1}".format("ganesh","Sella"))
+##we can also use variables
+print("I am {name} and my company is {company}".format(name="ganesh",company="Sella"))
+##when mixing both
+#WRONG print("I am {name} and my company is {0}".format(name="ganesh, Sella"))
+print("I am {name} and my company is {0}".format("Sella",name="ganesh"))
+print("###################")
+##################################################
+#alignment
+#formating binary and hexadecimal and octal
+##left alignment <
+##right alignment >
+## ' inside the strings can be used with double quotes
+print('{:>20}'.format("World")) #the number denotes if the characters are of that many or lesser length, it would align else it would ignore.
+print('{:b}'.format(12))
+print('{:x}'.format(12))
+print('{:o}'.format(12))
+print("###################")
+##################################################
+#Setting up a webserver using python.
+#just in command line, type python -m http.server 8889
+print("###################")
+##################################################
